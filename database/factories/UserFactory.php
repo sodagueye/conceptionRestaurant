1fd139c6e57cc34aa2840b6ff8445da16be4abfc
h4r0n1 <mdl.diop97@gmail.com>
2025-05-20 17:40:45 +0000

    fixed filament issue

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -24,10 +24,13 @@ class UserFactory extends Factory
     public function definition(): array
     {
         return [
-            'name' => fake()->name(),
-            'email' => fake()->unique()->safeEmail(),
+            'name' => 'Gueye',
+            'prenom' => 'Soda',
+            'email' => 'soda@gmail.com',
+            'telephone' => '780000000',
+            'role' => 'admin',
             'email_verified_at' => now(),
-            'password' => static::$password ??= Hash::make('password'),
+            'password' => static::$password ??= Hash::make('passer123'),
             'remember_token' => Str::random(10),
         ];
     }
